9c5af2921ba2738719d65879439c462126416581
Juan Navarro <jupanze@gmail.com>
2021-08-15 08:54:08 -0500

    Instalacion App y conexion BD

--- pedidosProject/settings.py
+++ pedidosProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'pedidosApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'pedidosProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'ec2-3-217-68-126.compute-1.amazonaws.com',
+        'NAME': 'dccfah2lft4pu4',
+        'USER': 'gdmtvdxwxvlvjb',
+        'PORT': '5432',
+        'PASSWORD': 'a6c94adfb1deacf5e76740fe79861730cdeebfc508d52b149ef56bd5e378f3bd',
     }
 }
 
